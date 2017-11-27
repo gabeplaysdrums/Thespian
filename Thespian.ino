c42edbb6cf41cd0d35b667457a5847a2efc2bd6a
Gabriel Young <gabeplaysdrums@live.com>
2017-11-27 11:21:02 -0800

    fix bugs

--- Thespian.ino
+++ Thespian.ino
@@ -10,6 +10,8 @@
 //
 // This example code is in the public domain.
 
+#define AUDIO_RECORD_SAMPLE_RATE 44100
+
 #include <Bounce.h>
 #include <Audio.h>
 #include <Wire.h>
@@ -18,75 +20,39 @@
 #include <SerialFlash.h>
 #include "record_sd_wav.h"
 
-#if defined(__MKL26Z64__)
-    #warning("[gabe] __MKL26Z64__")
-#elif defined(__MK20DX128__)
-    #warning("[gabe] __MK20DX128__")
-#elif defined(__MK20DX256__)
-    #warning("[gabe] __MK20DX256__")
-#elif defined(__MK64FX512__)
-    #warning("[gabe] __MK64FX512__")
-#elif defined(__MK66FX1M0__)
-    #warning("[gabe] __MK66FX1M0__")
-#endif
+// Uncomment to test with a sine wave as input
+//#define TEST_INPUT_SINE 1
 
-class MyAudioRecordQueue : public AudioStream
-{
-public:
-    MyAudioRecordQueue(void) : AudioStream(1, inputQueueArray),
-        userblock(NULL), head(0), tail(0), enabled(0) { }
-    void begin(void) {
-        clear();
-        enabled = 1;
-        overruns = 0;
-    }
-    int available(void);
-    void clear(void);
-    int16_t * readBuffer(void);
-    void freeBuffer(void);
-    void end(void) {
-        enabled = 0;
-    }
-    virtual void update(void);
-
-    size_t GetOverrunCount() const
-    {
-        return overruns;
-    }
-private:
-    audio_block_t *inputQueueArray[1];
-    audio_block_t * volatile queue[53];
-    audio_block_t *userblock;
-    volatile uint8_t head, tail, enabled;
-    size_t overruns = 0;
-};
-
+#if TEST_INPUT_SINE
 // GUItool: begin automatically generated code
-AudioInputI2S            i2s2;           //xy=105,63
-AudioAnalyzePeak         peak1;          //xy=278,108
-MyAudioRecordQueue       queue1;         //xy=281,63
-MyAudioRecordQueue       queue2;
-AudioPlaySdRaw           playRaw1;       //xy=302,157
-AudioOutputI2S           i2s1;           //xy=470,120
-AudioRecordSdWavStereo   recWav1;
-AudioConnection          patchCord1(i2s2, 0, queue1, 0);
-AudioConnection          patchCord2(i2s2, 0, peak1, 0);
-AudioConnection          patchCord3(playRaw1, 0, i2s1, 0);
-AudioConnection          patchCord4(playRaw1, 0, i2s1, 1);
-AudioConnection          patchCord5(i2s2, 1, queue2, 0);
-AudioConnection          patchCord6(i2s2, 0, recWav1, 0);
-AudioConnection          patchCord7(i2s2, 1, recWav1, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=265,212
+AudioSynthWaveformSine   sine1;          //xy=351,437
+AudioRecordSdWavStereo   recWav1;          //xy=567,503
+AudioAnalyzePeak         peak1;          //xy=569,372
+AudioOutputI2S           i2s1;           //xy=571,439
+AudioConnection          patchCord1(sine1, peak1);
+AudioConnection          patchCord2(sine1, 0, i2s1, 0);
+AudioConnection          patchCord3(sine1, 0, i2s1, 1);
+AudioConnection          patchCord4(sine1, 0, recWav1, 0);
+AudioConnection          patchCord5(sine1, 0, recWav1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=319,765
 // GUItool: end automatically generated code
+#else
+// GUItool: begin automatically generated code
+AudioInputI2S            i2s2;           //xy=391,435
+AudioRecordSdWavStereo   recWav1;          //xy=567,503
+AudioAnalyzePeak         peak1;          //xy=569,372
+AudioOutputI2S           i2s1;           //xy=571,439
+AudioConnection          patchCord1(i2s2, 0, peak1, 0);
+AudioConnection          patchCord2(i2s2, 0, i2s1, 0);
+AudioConnection          patchCord3(i2s2, 0, recWav1, 0);
+AudioConnection          patchCord4(i2s2, 1, i2s1, 1);
+AudioConnection          patchCord5(i2s2, 1, recWav1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=319,765
+// GUItool: end automatically generated code
+#endif
 
 // For a stereo recording version, see this forum thread:
 // https://forum.pjrc.com/threads/46150?p=158388&viewfull=1#post158388
-
-// Bounce objects to easily and reliably read the buttons
-Bounce buttonRecord = Bounce(0, 8);
-Bounce buttonStop =   Bounce(1, 8);  // 8 = 8 ms debounce time
-Bounce buttonPlay =   Bounce(2, 8);
-
 
 // which input on the audio shield will be used?
 const int myInput = AUDIO_INPUT_LINEIN;
@@ -112,24 +78,26 @@ const int myInput = AUDIO_INPUT_LINEIN;
 // Remember which mode we're doing
 int mode = 0;  // 0=stopped, 1=recording, 2=playing
 
-// The file where data is recorded
-File frec;
-
 elapsedMillis recordingMillis;
 
 void setup() {
-  // Configure the pushbutton pins
-  pinMode(0, INPUT_PULLUP);
-  pinMode(1, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
+  Serial.begin(9600);
+  while (!Serial);
 
   // Audio connections require memory, and the record queue
   // uses this memory to buffer incoming audio.
-  AudioMemory(512);
+  AudioMemory(10);
 
   // Enable the audio shield, select input, and enable output
   sgtl5000_1.enable();
+
+#if TEST_INPUT_SINE
+  sine1.amplitude(1.0);
+  sine1.frequency(440.0);
+  sine1.phase(0);
+#else
   sgtl5000_1.inputSelect(myInput);
+#endif
   sgtl5000_1.volume(0.5);
 
   // Initialize the SD card
@@ -148,39 +116,15 @@ void setup() {
 
 
 void loop() {
-  // First, read the buttons
-  buttonRecord.update();
-  buttonStop.update();
-  buttonPlay.update();
-
-  // Respond to button presses
-  if (buttonRecord.fallingEdge()) {
-    Serial.println("Record Button Press");
-    if (mode == 2) stopPlaying();
-    if (mode == 0) startRecording();
-  }
-  if (buttonStop.fallingEdge()) {
-    Serial.println("Stop Button Press");
-    if (mode == 1) stopRecording();
-    if (mode == 2) stopPlaying();
-  }
-  if (buttonPlay.fallingEdge()) {
-    Serial.println("Play Button Press");
-    if (mode == 1) stopRecording();
-    if (mode == 0) startPlaying();
-  }
 
   // If we're playing or recording, carry on...
   if (mode == 1) {
     continueRecording();
 
-    if (recordingMillis > 30000)
+    if (recordingMillis > 600*1000)
     {
         stopRecording();
     }
-  }
-  if (mode == 2) {
-    continuePlaying();
   }
 
   // when using a microphone, continuously adjust gain
@@ -190,182 +134,33 @@ void loop() {
 
 void startRecording() {
   Serial.println("startRecording");
-  if (SD.exists("RECORD.RAW")) {
-    // The SD library writes new data to the end of the
-    // file, so to start a new recording, the old file
-    // must be deleted before new data is written.
-    SD.remove("RECORD.RAW");
-  }
-  frec = SD.open("RECORD.RAW", FILE_WRITE);
-  if (frec) {
-    queue1.begin();
-    queue2.begin();
-    mode = 1;
-  }
-
   recWav1.begin("RECORD.WAV");
-
+  mode = 1;
   recordingMillis = 0;
 }
 
 void continueRecording() {
-  if (queue1.available() > 0 && queue2.available() > 0) {
-    byte buffer[512];
-    // Fetch 2 blocks from the audio library and copy
-    // into a 512 byte buffer.  The Arduino SD library
-    // is most efficient when full 512 byte sector size
-    // writes are used.
-    memcpy(buffer, queue1.readBuffer(), 256);
-    queue1.freeBuffer();
-    memcpy(buffer+256, queue2.readBuffer(), 256);
-    queue2.freeBuffer();
-    // write all 512 bytes to the SD card
-    elapsedMicros usec = 0;
-    frec.write(buffer, 512);
-    // Uncomment these lines to see how long SD writes
-    // are taking.  A pair of audio blocks arrives every
-    // 5802 microseconds, so hopefully most of the writes
-    // take well under 5802 us.  Some will take more, as
-    // the SD library also must write to the FAT tables
-    // and the SD card controller manages media erase and
-    // wear leveling.  The queue1 object can buffer
-    // approximately 301700 us of audio, to allow time
-    // for occasional high SD card latency, as long as
-    // the average write time is under 5802 us.
-    Serial.print("SD write, us=");
-    Serial.println(usec);
-
-  }
-
   recWav1.process();
 }
 
 void stopRecording() {
   Serial.println("stopRecording");
-  queue1.end();
-  queue2.end();
-  if (mode == 1) {
-    while (queue1.available() > 0 && queue2.available() > 0) {
-      frec.write((byte*)queue1.readBuffer(), 256);
-      queue1.freeBuffer();
-      frec.write((byte*)queue2.readBuffer(), 256);
-      queue2.freeBuffer();
-    }
-    frec.close();
-
-    Serial.print("overruns: ");
-    Serial.println(queue1.GetOverrunCount());
-  }
-
   recWav1.end();
+#if TEST_INPUT_SINE
+  sine1.amplitude(0);
+#endif
   mode = 0;
-}
-
-
-void startPlaying() {
-  Serial.println("startPlaying");
-  playRaw1.play("RECORD.RAW");
-  mode = 2;
-}
-
-void continuePlaying() {
-  if (!playRaw1.isPlaying()) {
-    playRaw1.stop();
-    mode = 0;
-  }
-}
-
-void stopPlaying() {
-  Serial.println("stopPlaying");
-  if (mode == 2) playRaw1.stop();
-  mode = 0;
+  Serial.print("Valid blocks: ");
+  Serial.println(recWav1.validBlockCount());
+  Serial.print("Dropped blocks: ");
+  Serial.println(recWav1.droppedBlockCount());
+  Serial.print("Partial blocks: ");
+  Serial.println(recWav1.partialBlockCount());
+  Serial.print("Max pending samples: ");
+  Serial.println(recWav1.maxPendingSampleCount());
 }
 
 void adjustMicLevel() {
   // TODO: read the peak1 object and adjust sgtl5000_1.micGain()
   // if anyone gets this working, please submit a github pull request :-)
-}
-
-
-
-
-
-#include "utility/dspinst.h"
-
-int MyAudioRecordQueue::available(void)
-{
-    uint32_t h, t;
-
-    h = head;
-    t = tail;
-    if (h >= t) return h - t;
-    return 53 + h - t;
-}
-
-void MyAudioRecordQueue::clear(void)
-{
-    uint32_t t;
-
-    if (userblock) {
-        release(userblock);
-        userblock = NULL;
-    }
-    t = tail;
-    while (t != head) {
-        if (++t >= 53) t = 0;
-        release(queue[t]);
-    }
-    tail = t;
-}
-
-int16_t * MyAudioRecordQueue::readBuffer(void)
-{
-    uint32_t t;
-
-    if (userblock) return NULL;
-    t = tail;
-    if (t == head) return NULL;
-    if (++t >= 53) t = 0;
-    // Serial.print("readBuffer.  available: ");
-    // Serial.println(available());
-    userblock = queue[t];
-    tail = t;
-    return userblock->data;
-}
-
-void MyAudioRecordQueue::freeBuffer(void)
-{
-    if (userblock == NULL) return;
-    release(userblock);
-    userblock = NULL;
-    // Serial.println("freeBuffer");
-}
-
-void MyAudioRecordQueue::update(void)
-{
-    audio_block_t *block;
-    uint32_t h;
-
-    block = receiveReadOnly();
-    if (!block) return;
-    if (!enabled) {
-        release(block);
-        return;
-    }
-    h = head + 1;
-    if (h >= 53)
-    {
-        h = 0;
-        // Serial.println("Wrapping around ring buffer");
-    }
-
-    if (h == tail) {
-        release(block);
-        // Serial.print("Buffer dropped.  available: ");
-        // Serial.println(available());
-        overruns++;
-    } else {
-        queue[h] = block;
-        head = h;
-    }
 }
--- record_sd_wav.h
+++ record_sd_wav.h
@@ -6,6 +6,19 @@
 #include "SD.h"
 #include "memcpy_audio.h"
 
+// Debugging
+
+// Uncomment to debug AudioRecordSdWav
+// #define DEBUG_AUDIO_RECORD_SD_WAV 1
+// Uncomment to debug AudioRecordSdWav::update
+// #define DEBUG_AUDIO_RECORD_SD_WAV_UPDATE 1
+// Uncomment to debug AudioRecordSdWav::write
+// #define DEBUG_AUDIO_RECORD_SD_WAV_WRITE 1
+
+#ifndef AUDIO_RECORD_SAMPLE_RATE
+#define AUDIO_RECORD_SAMPLE_RATE AUDIO_SAMPLE_RATE
+#endif
+
 template<unsigned char ninput>
 class AudioRecordSdWav : public AudioStream
 {
@@ -17,32 +30,40 @@ public:
     void process();
 
     // queue should be large enough to accommodate long SD writes
-    static constexpr uint8_t InterleavedQueueLengthMillis = 100;
-    static constexpr uint16_t InterleavedQueueBlockCount = static_cast<uint16_t>(
-        InterleavedQueueLengthMillis *
-        static_cast<size_t>(AUDIO_SAMPLE_RATE)/*sampls/sec*/ /
+    static constexpr uint16_t InterleavedQueueLengthMillis = 1000;
+    static constexpr uint16_t InterleavedQueueBlockCount = static_cast<uint16_t>(ceil(
+        static_cast<float>(InterleavedQueueLengthMillis) *
+        AUDIO_RECORD_SAMPLE_RATE/*samples/sec*/ /
         1000/*millis/sec*/ /
-        AUDIO_BLOCK_SAMPLES);
-    static constexpr uint16_t InterleavedQueueSampleCount = (
+        AUDIO_BLOCK_SAMPLES /*samples/block*/));
+    static constexpr uint32_t InterleavedQueueSampleCount = (
         ninput *
         AUDIO_BLOCK_SAMPLES *
         InterleavedQueueBlockCount);
 
-    uint16_t getDroppedBlockCount() const { return droppedBlocks; }
+    uint32_t validBlockCount() const { return validBlocks; }
+    uint32_t droppedBlockCount() const { return droppedBlocks; }
+    uint32_t partialBlockCount() const { return partialBlocks; }
+    uint32_t maxPendingSampleCount() const { return maxPendingSamples; }
 
 private:
     void update() override;
-    uint16_t pendingInterleavedSamples() const;
+    uint32_t pendingInterleavedSamples() const;
     bool interleavedQueueHasCapacity() const;
-    void drain(uint16_t samples);
+    void write(uint32_t sampleCount);
 
     const uint16_t FileBlockSampleCount = 512 / sizeof(int16_t);
     audio_block_t* inputQueueArray[ninput];
     int16_t interleavedQueue[InterleavedQueueSampleCount];
-    volatile uint16_t head = 0;
-    volatile uint16_t tail = 0;
+    volatile uint32_t head = 0;
+    volatile uint32_t tail = 0;
+    volatile bool enabled = false;
     File file;
-    uint16_t droppedBlocks = 0;
+
+    uint32_t validBlocks = 0;
+    uint32_t droppedBlocks = 0;
+    uint32_t partialBlocks = 0;
+    uint32_t maxPendingSamples = 0;
     uint32_t dataChunkPos = 0;
 };
 
@@ -61,7 +82,10 @@ namespace {
 
     template<>
     inline void copySamplesInterleaved<2>(audio_block_t* src[2], int16_t* dst) {
+        // memcpy_tointerleaveLR assumes the output buffer has length AUDIO_BLOCK_SAMPLES,
+        // so we need to copy each half separately
         memcpy_tointerleaveLR(dst, src[0]->data, src[1]->data);
+        memcpy_tointerleaveLR(dst + AUDIO_BLOCK_SAMPLES, src[0]->data + AUDIO_BLOCK_SAMPLES/2, src[1]->data + AUDIO_BLOCK_SAMPLES/2);
     }
 
     static bool detectLittleEndian()
@@ -90,6 +114,16 @@ namespace {
 
 template<unsigned char ninput>
 void AudioRecordSdWav<ninput>::begin(const char* filename) {
+#if DEBUG_AUDIO_RECORD_SD_WAV
+    Serial.print("AudioRecordSdWav::begin(\"");
+    Serial.print(filename);
+    Serial.println("\")");
+#endif
+
+    if (SD.exists(filename)) {
+        SD.remove(filename);
+    }
+
     file = SD.open(filename, FILE_WRITE);
     if (!file) return;
 
@@ -100,32 +134,71 @@ void AudioRecordSdWav<ninput>::begin(const char* filename) {
     writeWordLittleEndian(file, 16, 4);
     // PCM - integer samples
     writeWordLittleEndian(file, 1, 2);
+
     // number of channels
-    writeWordLittleEndian(file, ninput, 2);
+    constexpr uint16_t channelCount = ninput;
+#if DEBUG_AUDIO_RECORD_SD_WAV
+    Serial.print("Number of channels: ");
+    Serial.println(channelCount);
+#endif
+    writeWordLittleEndian(file, channelCount, 2);
+
     // samples per second (Hz)
-    writeWordLittleEndian(file, static_cast<uint32_t>(AUDIO_SAMPLE_RATE), 4);
+    constexpr uint32_t sampleRate = static_cast<uint32_t>(AUDIO_RECORD_SAMPLE_RATE);
+#if DEBUG_AUDIO_RECORD_SD_WAV
+    Serial.print("Sample rate: ");
+    Serial.println(sampleRate);
+#endif
+    writeWordLittleEndian(file, sampleRate, 4);
+
     // (Sample Rate * BitsPerSample * Channels) / 8
-    writeWordLittleEndian(file, static_cast<uint32_t>(AUDIO_SAMPLE_RATE) * sizeof(int16_t) * ninput / 8, 4);
+    constexpr uint16_t bitsPerSample = 8 * sizeof(int16_t);
+    constexpr uint32_t dataRate = sampleRate * bitsPerSample * channelCount / 8;
+#if DEBUG_AUDIO_RECORD_SD_WAV
+    Serial.print("Data rate: ");
+    Serial.println(dataRate);
+#endif
+    writeWordLittleEndian(file, dataRate, 4);
+
     // data block size (size of two integer samples, one for each channel, in bytes)
-    writeWordLittleEndian(file, sizeof(int16_t) * ninput, 2);
+    constexpr uint16_t blockSize = sizeof(int16_t) * ninput;
+#if DEBUG_AUDIO_RECORD_SD_WAV
+    Serial.print("Block size: ");
+    Serial.println(blockSize);
+#endif
+    writeWordLittleEndian(file, blockSize, 2);
+
     // number of bits per sample (use a multiple of 8)
-    writeWordLittleEndian(file, 8 * sizeof(int16_t), 2);
+#if DEBUG_AUDIO_RECORD_SD_WAV
+    Serial.print("Bits per sample: ");
+    Serial.println(bitsPerSample);
+#endif
+    writeWordLittleEndian(file, bitsPerSample, 2);
 
     dataChunkPos = file.position();
 
     constexpr char DataChunk[] = "data----";
     file.write(DataChunk, sizeof(DataChunk) - 1); // do not write string null terminator
+
+    head = 0;
+    tail = 0;
+    validBlocks = 0;
+    partialBlocks = 0;
+    droppedBlocks = 0;
+    maxPendingSamples = 0;
+    enabled = true;
 }
 
 template<unsigned char ninput>
 void AudioRecordSdWav<ninput>::end() {
-    if (!file) return;
+    if (!enabled) return;
+    enabled = false;
 
     while (pendingInterleavedSamples() > FileBlockSampleCount) {
-        drain(FileBlockSampleCount);
+        write(FileBlockSampleCount);
     }
 
-    drain(pendingInterleavedSamples());
+    write(pendingInterleavedSamples());
 
     // (We'll need the final file size to fix the chunk sizes above)
     const uint32_t fileLength = file.position();
@@ -144,22 +217,55 @@ void AudioRecordSdWav<ninput>::end() {
 
 template<unsigned char ninput>
 void AudioRecordSdWav<ninput>::process() {
-    drain(FileBlockSampleCount);
+    write(FileBlockSampleCount);
 }
 
 template<unsigned char ninput>
-void AudioRecordSdWav<ninput>::drain(uint16_t sampleCount) {
-    if (pendingInterleavedSamples() < sampleCount) return;
+void AudioRecordSdWav<ninput>::write(uint32_t sampleCount) {
+#if DEBUG_AUDIO_RECORD_SD_WAV_WRITE
+    Serial.print("AudioRecordSdWav::write(");
+    Serial.print(sampleCount);
+    Serial.println(")");
+#endif
 
     uint32_t h = head, t = tail;
+    uint32_t pendingSamples = pendingInterleavedSamples();
+
+#if DEBUG_AUDIO_RECORD_SD_WAV_WRITE
+    Serial.print("head: ");
+    Serial.print(h);
+    Serial.print(" tail: ");
+    Serial.print(t);
+    Serial.print(" pending count: ");
+    Serial.println(pendingSamples);
+#endif
+
+    if (pendingSamples < sampleCount) return;
 
     if (h < t && InterleavedQueueSampleCount - t < sampleCount) {
         // Chunk is not contiguous.  Write two smaller chunks
-        file.write(reinterpret_cast<const uint8_t*>(interleavedQueue + t), (InterleavedQueueSampleCount - t) * sizeof(int16_t));
-        file.write(reinterpret_cast<const uint8_t*>(interleavedQueue), (sampleCount - (InterleavedQueueSampleCount - t)) * sizeof(int16_t));
+        const uint32_t sampleCount1 = InterleavedQueueSampleCount - t;
+        const uint32_t sampleCount2 = sampleCount - sampleCount1;
+
+#if DEBUG_AUDIO_RECORD_SD_WAV_WRITE
+        Serial.print("Chunk is not contiguous. Doing 2 writes: ");
+        Serial.print(sampleCount1);
+        Serial.print(" ");
+        Serial.println(sampleCount2);
+#endif
+
+        file.write(reinterpret_cast<const uint8_t*>(interleavedQueue + t), sampleCount1 * sizeof(int16_t));
+        file.write(reinterpret_cast<const uint8_t*>(interleavedQueue), sampleCount2 * sizeof(int16_t));
     }
     else {
         // Chunk is contiguous, Write it to the file
+
+#if DEBUG_AUDIO_RECORD_SD_WAV_WRITE
+        Serial.print("Writing ");
+        Serial.print(sampleCount);
+        Serial.println(" samples");
+#endif
+
         file.write(reinterpret_cast<const uint8_t*>(interleavedQueue + t), sampleCount * sizeof(int16_t));
     }
 
@@ -173,46 +279,109 @@ void AudioRecordSdWav<ninput>::drain(uint16_t sampleCount) {
 
 template<unsigned char ninput>
 void AudioRecordSdWav<ninput>::update() {
-    if (!file) return;
+    if (!enabled) return;
+
+#if DEBUG_AUDIO_RECORD_SD_WAV_UPDATE
+    Serial.println("AudioRecordSdWav::update()");
+#endif
 
     audio_block_t* input[ninput];
+    uint8_t receivedCount = 0;
 
     for (unsigned char channel=0; channel < ninput; channel++) {
         input[channel] = receiveReadOnly(channel);
 
         if (!input[channel]) {
-            // failed to receive buffer.  clean up and exit
-            for (unsigned char releaseChannel=0; releaseChannel < channel; releaseChannel++) {
-                release(input[releaseChannel]);
-            }
-            return;
+#if DEBUG_AUDIO_RECORD_SD_WAV_UPDATE
+            Serial.print("Failed to receive block from channel ");
+            Serial.println(channel);
+#endif
+            continue;
         }
+
+        receivedCount++;
+
+#if DEBUG_AUDIO_RECORD_SD_WAV_UPDATE
+        Serial.print("Received block from channel ");
+        Serial.print(channel);
+        Serial.print(". first: ");
+        Serial.print(*(input[channel]->data));
+        Serial.print(" last: ");
+        Serial.println(*(input[channel]->data + AUDIO_BLOCK_SAMPLES - 1));
+#endif
     }
 
-    if (interleavedQueueHasCapacity()) {
-        uint32_t h = head;
+    uint32_t h = head;
+    uint32_t pendingSamples = pendingInterleavedSamples();
 
-        copySamplesInterleaved<ninput>(input, interleavedQueue + h);
+#if DEBUG_AUDIO_RECORD_SD_WAV_UPDATE
+    uint32_t t = tail;
+    Serial.print("head: ");
+    Serial.print(h);
+    Serial.print(" tail: ");
+    Serial.print(t);
+    Serial.print(" pending count: ");
+    Serial.print(pendingSamples);
+    Serial.print(" (max ");
+    Serial.print(InterleavedQueueSampleCount);
+    Serial.println(")");
+#endif
 
-        // advance head
-        h += (ninput * AUDIO_BLOCK_SAMPLES);
-        if (h >= InterleavedQueueSampleCount) {
-            h -= InterleavedQueueSampleCount;
+    if (pendingSamples > maxPendingSamples) {
+        maxPendingSamples = pendingSamples;
+    }
+
+    if (receivedCount < ninput) {
+        // not enough data received
+
+        if (ninput > 1 && receivedCount != 0) {
+            partialBlocks++;
         }
-        head = h;
     }
     else {
-        // queue is not draining fast enough.  drop incoming samples
-        droppedBlocks++;
+        validBlocks++;
+
+        if (interleavedQueueHasCapacity()) {
+            copySamplesInterleaved<ninput>(input, interleavedQueue + h);
+
+#if DEBUG_AUDIO_RECORD_SD_WAV_UPDATE
+            Serial.print("Copied samples.  first: ");
+            Serial.print(*(interleavedQueue + h));
+            Serial.print(" ");
+            Serial.print(*(interleavedQueue + h + 1));
+            Serial.print(" last: ");
+            Serial.print(*(interleavedQueue + h + AUDIO_BLOCK_SAMPLES * ninput - 2));
+            Serial.print(" ");
+            Serial.println(*(interleavedQueue + h + AUDIO_BLOCK_SAMPLES * ninput - 1));
+#endif
+
+            // advance head
+            h += (ninput * AUDIO_BLOCK_SAMPLES);
+            if (h >= InterleavedQueueSampleCount) {
+                h -= InterleavedQueueSampleCount;
+            }
+            head = h;
+        }
+        else {
+            // queue is not draining fast enough.  drop incoming samples
+            droppedBlocks++;
+
+#if DEBUG_AUDIO_RECORD_SD_WAV_UPDATE
+            Serial.print("Audio block dropped! total: ");
+            Serial.println(droppedBlocks);
+#endif
+        }
     }
 
     for (unsigned char channel=0; channel < ninput; channel++) {
-        release(input[channel]);
+        if (input[channel]) {
+            release(input[channel]);
+        }
     }
 }
 
 template<unsigned char ninput>
-uint16_t AudioRecordSdWav<ninput>::pendingInterleavedSamples() const {
+uint32_t AudioRecordSdWav<ninput>::pendingInterleavedSamples() const {
     uint32_t h = head, t = tail;
 
     if (h < t) {
