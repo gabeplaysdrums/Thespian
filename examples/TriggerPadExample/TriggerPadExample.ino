b011c243b0b44bfbcc8efef716ed2698b9759a85
Gabriel Young <gabeplaysdrums@live.com>
2017-12-10 19:57:41 -0800

    read analog input

--- examples/TriggerPadExample/TriggerPadExample.ino
+++ examples/TriggerPadExample/TriggerPadExample.ino
@@ -1,9 +1,34 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+bool active = false;
+elapsedMillis activeMillis;
+int peak = 0;
+elapsedMillis peakMillis;
 
+void loop() {
+  const auto value = analogRead(A0);
+//#if 0
+  peak = max(peak, value);
+  if (peakMillis > 20) {
+    peakMillis = 0;
+    Serial.println(peak);
+    peak = 0;
+  }
+  delay(10);
+//#endif
+#if 0
+  if (active) {
+    Serial.println(value);
+
+    if (activeMillis > 500) {
+      active = false;
+    }
+  }
+  else if (value > 200) {
+    active = true;
+    activeMillis = 0;
+  }
+#endif
 }
